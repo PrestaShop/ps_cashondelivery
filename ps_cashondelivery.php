63c123477b1fdba8af87d86818f0c2e93527920c
Antoine Mille <antoine.mille.site@gmail.com>
2016-10-28 09:45:30 +0200

    CO: delete duplicate code

--- ps_cashondelivery.php
+++ ps_cashondelivery.php
@@ -41,10 +41,7 @@ class Ps_Cashondelivery extends PaymentModule
 		$this->need_instance = 1;
 		$this->controllers = array('validation');
 		$this->is_eu_compatible = 1;
-
 		$this->currencies = false;
-
-        $this->ps_versions_compliancy = array('min' => '1.7.0.0', 'max' => _PS_VERSION_);
 
 		parent::__construct();
 
