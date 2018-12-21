0f5f63064dc1a488115eb2d6974546e17665bfb7
MathiasReker <mathias@reker.dk>
2018-12-21 02:01:41 +0100

    Use short array syntax

--- ps_cashondelivery.php
+++ ps_cashondelivery.php
@@ -39,18 +39,18 @@ class Ps_Cashondelivery extends PaymentModule
         $this->version = '1.0.6';
         $this->need_instance = 1;
 
-        $this->ps_versions_compliancy = array('min' => '1.7.1.0', 'max' => _PS_VERSION_);
-        $this->controllers = array('validation');
+        $this->ps_versions_compliancy = ['min' => '1.7.1.0', 'max' => _PS_VERSION_];
+        $this->controllers = ['validation'];
         $this->is_eu_compatible = 1;
         $this->currencies = false;
 
         parent::__construct();
 
-        $this->displayName = $this->trans('Cash on delivery (COD)', array(), 'Modules.Cashondelivery.Admin');
-        $this->description = $this->trans('Accept cash on delivery payments', array(), 'Modules.Cashondelivery.Admin');
+        $this->displayName = $this->trans('Cash on delivery (COD)', [], 'Modules.Cashondelivery.Admin');
+        $this->description = $this->trans('Accept cash on delivery payments', [], 'Modules.Cashondelivery.Admin');
 
         /* For 1.4.3 and less compatibility */
-        $updateConfig = array('PS_OS_CHEQUE', 'PS_OS_PAYMENT', 'PS_OS_PREPARATION', 'PS_OS_SHIPPING', 'PS_OS_CANCELED', 'PS_OS_REFUND', 'PS_OS_ERROR', 'PS_OS_OUTOFSTOCK', 'PS_OS_BANKWIRE', 'PS_OS_PAYPAL', 'PS_OS_WS_PAYMENT');
+        $updateConfig = ['PS_OS_CHEQUE', 'PS_OS_PAYMENT', 'PS_OS_PREPARATION', 'PS_OS_SHIPPING', 'PS_OS_CANCELED', 'PS_OS_REFUND', 'PS_OS_ERROR', 'PS_OS_OUTOFSTOCK', 'PS_OS_BANKWIRE', 'PS_OS_PAYPAL', 'PS_OS_WS_PAYMENT'];
         if (!Configuration::get('PS_OS_PAYMENT')) {
             foreach ($updateConfig as $u) {
                 if (!Configuration::get($u) && defined('_'.$u.'_')) {
@@ -97,8 +97,8 @@ class Ps_Cashondelivery extends PaymentModule
 
         $newOption = new PaymentOption();
         $newOption->setModuleName($this->name)
-            ->setCallToActionText($this->trans('Pay by Cash on Delivery', array(), 'Modules.Cashondelivery.Shop'))
-            ->setAction($this->context->link->getModuleLink($this->name, 'validation', array(), true))
+            ->setCallToActionText($this->trans('Pay by Cash on Delivery', [], 'Modules.Cashondelivery.Shop'))
+            ->setAction($this->context->link->getModuleLink($this->name, 'validation', [], true))
             ->setAdditionalInformation($this->fetch('module:ps_cashondelivery/views/templates/hook/ps_cashondelivery_intro.tpl'));
 
         $payment_options = [
@@ -116,7 +116,7 @@ class Ps_Cashondelivery extends PaymentModule
         $state = $params['order']->getCurrentState();
 
         if ($state) {
-            $this->smarty->assign(array(
+            $this->smarty->assign([
                 'shop_name' => $this->context->shop->name,
                 'total' => Tools::displayPrice(
                     $params['order']->getOrdersTotalPaid(),
@@ -126,12 +126,12 @@ class Ps_Cashondelivery extends PaymentModule
                 'status' => 'ok',
                 'reference' => $params['order']->reference,
                 'contact_url' => $this->context->link->getPageLink('contact', true)
-            ));
+            ]);
         } else {
-            $this->smarty->assign(array(
+            $this->smarty->assign([
                 'status' => 'failed',
                 'contact_url' => $this->context->link->getPageLink('contact', true)
-            ));
+            ]);
         }
         return $this->fetch('module:ps_cashondelivery/views/templates/hook/payment_return.tpl');
     }
