7f1b4426ace6ab2643c57df31e40384798864eb0
MathiasReker <mathias@reker.dk>
2018-12-23 04:26:55 +0100

    Trailing comma in multiline array

--- ps_cashondelivery.php
+++ ps_cashondelivery.php
@@ -125,12 +125,12 @@ class Ps_Cashondelivery extends PaymentModule
                 ),
                 'status' => 'ok',
                 'reference' => $params['order']->reference,
-                'contact_url' => $this->context->link->getPageLink('contact', true)
+                'contact_url' => $this->context->link->getPageLink('contact', true),
             ));
         } else {
             $this->smarty->assign(array(
                 'status' => 'failed',
-                'contact_url' => $this->context->link->getPageLink('contact', true)
+                'contact_url' => $this->context->link->getPageLink('contact', true),
             ));
         }
         return $this->fetch('module:ps_cashondelivery/views/templates/hook/payment_return.tpl');
