3c51f7de253e1aa3562c3ca003c315c9559eabe3
MathiasReker <mathias@reker.dk>
2018-12-27 01:22:39 +0100

    One concat space

--- controllers/front/validation.php
+++ controllers/front/validation.php
@@ -34,7 +34,7 @@ class Ps_CashondeliveryValidationModuleFrontController extends ModuleFrontContro
     public function postProcess()
     {
         if ($this->context->cart->id_customer == 0 || $this->context->cart->id_address_delivery == 0 || $this->context->cart->id_address_invoice == 0 || !$this->module->active) {
-            Tools::redirectLink(__PS_BASE_URI__.'order.php?step=1');
+            Tools::redirectLink(__PS_BASE_URI__ . 'order.php?step=1');
         }
 
         // Check that this payment option is still available in case the customer changed his address just before the end of the checkout process
@@ -50,11 +50,11 @@ class Ps_CashondeliveryValidationModuleFrontController extends ModuleFrontContro
         }
         $customer = new Customer($this->context->cart->id_customer);
         if (!Validate::isLoadedObject($customer)) {
-            Tools::redirectLink(__PS_BASE_URI__.'order.php?step=1');
+            Tools::redirectLink(__PS_BASE_URI__ . 'order.php?step=1');
         }
         $customer = new Customer((int)$this->context->cart->id_customer);
         $total = $this->context->cart->getOrderTotal(true, Cart::BOTH);
         $this->module->validateOrder((int)$this->context->cart->id, Configuration::get('PS_OS_PREPARATION'), $total, $this->module->displayName, null, array(), null, false, $customer->secure_key);
-        Tools::redirectLink(__PS_BASE_URI__.'order-confirmation.php?key='.$customer->secure_key.'&id_cart='.(int)$this->context->cart->id.'&id_module='.(int)$this->module->id.'&id_order='.(int)$this->module->currentOrder);
+        Tools::redirectLink(__PS_BASE_URI__ . 'order-confirmation.php?key=' . $customer->secure_key . '&id_cart=' . (int)$this->context->cart->id . '&id_module=' . (int)$this->module->id . '&id_order=' . (int)$this->module->currentOrder);
     }
 }
