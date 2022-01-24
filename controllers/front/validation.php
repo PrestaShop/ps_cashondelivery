768dc12e94e98d821ee83db341fbd0c2c7816d5c
Progi1984 <franck.lefevre@prestashop.com>
2022-01-24 16:31:30 +0100

    Fixed CI

--- controllers/front/validation.php
+++ controllers/front/validation.php
@@ -26,6 +26,8 @@
 
 /**
  * @since 1.5.0
+ *
+ * @property Ps_Cashondelivery $module
  */
 class Ps_CashondeliveryValidationModuleFrontController extends ModuleFrontController
 {
@@ -34,7 +36,7 @@ class Ps_CashondeliveryValidationModuleFrontController extends ModuleFrontContro
     public function postProcess()
     {
         if ($this->context->cart->id_customer == 0 || $this->context->cart->id_address_delivery == 0 || $this->context->cart->id_address_invoice == 0 || !$this->module->active) {
-            Tools::redirectLink(__PS_BASE_URI__.'order.php?step=1');
+            Tools::redirectLink(__PS_BASE_URI__ . 'order.php?step=1');
         }
 
         // Check that this payment option is still available in case the customer changed his address just before the end of the checkout process
@@ -46,15 +48,25 @@ class Ps_CashondeliveryValidationModuleFrontController extends ModuleFrontContro
             }
         }
         if (!$authorized) {
-            die(Tools::displayError('This payment method is not available.'));
+            exit(Tools::displayError('This payment method is not available.'));
         }
-        $customer = new Customer((int)$this->context->cart->id_customer);
+        $customer = new Customer((int) $this->context->cart->id_customer);
         if (!Validate::isLoadedObject($customer)) {
-            Tools::redirectLink(__PS_BASE_URI__.'order.php?step=1');
+            Tools::redirectLink(__PS_BASE_URI__ . 'order.php?step=1');
         }
 
         $total = $this->context->cart->getOrderTotal(true, Cart::BOTH);
-        $this->module->validateOrder((int)$this->context->cart->id, Configuration::get('PS_OS_PREPARATION'), $total, $this->module->displayName, null, array(), null, false, $customer->secure_key);
-        Tools::redirectLink(__PS_BASE_URI__.'order-confirmation.php?key='.$customer->secure_key.'&id_cart='.(int)$this->context->cart->id.'&id_module='.(int)$this->module->id.'&id_order='.(int)$this->module->currentOrder);
+        $this->module->validateOrder(
+            (int) $this->context->cart->id,
+            (int) Configuration::get('PS_OS_PREPARATION'),
+            $total,
+            $this->module->displayName,
+            null,
+            [],
+            null,
+            false,
+            $customer->secure_key
+        );
+        Tools::redirectLink(__PS_BASE_URI__ . 'order-confirmation.php?key=' . $customer->secure_key . '&id_cart=' . (int) $this->context->cart->id . '&id_module=' . (int) $this->module->id . '&id_order=' . (int) $this->module->currentOrder);
     }
 }
