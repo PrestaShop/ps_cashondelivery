bf3dbe7562ecdc4748f61f70eaf2d62afacdd5c8
Progi1984 <franck.lefevre@prestashop.com>
2022-06-02 12:06:09 +0200

    Bump version to 2.0.1

--- ps_cashondelivery.php
+++ ps_cashondelivery.php
@@ -44,7 +44,7 @@ class Ps_Cashondelivery extends PaymentModule
         $this->name = 'ps_cashondelivery';
         $this->tab = 'payments_gateways';
         $this->author = 'PrestaShop';
-        $this->version = '2.0.0';
+        $this->version = '2.0.1';
         $this->need_instance = 1;
         $this->ps_versions_compliancy = ['min' => '1.7.6.0', 'max' => _PS_VERSION_];
         $this->controllers = ['validation'];
